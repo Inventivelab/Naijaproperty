917d970e8e21601e206317d6ae6f20e1dbbc4cbc
Said Maadan <me@saidmaadan.com>
2018-05-09 09:21:01 -0500

    Updated javascript/application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require rails-ujs
+//= require active_admin
 //= require jquery-ui/widgets/datepicker
 //= require jquery-ui/widgets/slider
 //= require jquery-ui/widgets/autocomplete
